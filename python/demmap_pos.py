58fdc8b8d3c5a4c8133d1c4a687744245ce11d47
nawinnova <nawinnga@gmail.com>
2023-11-08 17:08:00 +0000

    change max workers=64

--- python/demmap_pos.py
+++ python/demmap_pos.py
@@ -110,6 +110,9 @@ def demmap_pos(dd,ed,rmatrix,logt,dlogt,glc,reg_tweak=1.0,max_iter=10,rgt_fact=1
     kdagk=np.zeros([nt,nt])
     dn_reg=np.zeros([na,nf])
     ednin=np.zeros([nf])
+
+    # set context for parallelisation
+
  
     # do we have enough DEM's to make parallel make sense?
     if (na>=200):
@@ -118,7 +121,7 @@ def demmap_pos(dd,ed,rmatrix,logt,dlogt,glc,reg_tweak=1.0,max_iter=10,rgt_fact=1
 #       Put this here to make sure running dem calc in parallel, not the underlying np/gsvd stuff (this correct/needed?)  
         with threadpool_limits(limits=1):
             #Add mac number of cpus here
-            with ProcessPoolExecutor(max_workers=100) as exe:
+            with ProcessPoolExecutor(max_workers=64) as exe:
                 futures=[exe.submit(dem_unwrap, dd[i*n_par:(i+1)*n_par,:],ed[i*n_par:(i+1)*n_par,:],rmatrix,logt,dlogt,glc, \
                     reg_tweak=reg_tweak,max_iter=max_iter,rgt_fact=rgt_fact,dem_norm0=dem_norm0[i*n_par:(i+1)*n_par,:],\
                         nmu=nmu,warn=warn,l_emd=l_emd) \
